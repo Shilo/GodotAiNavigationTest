471762b59034f7992c1027e74bda822602debbfa
Shilo <shilo86@gmail.com>
2024-03-16 22:53:25 -0700

    feat: setup basic navigation handling

--- entity/entity.gd
+++ entity/entity.gd
@@ -1,7 +1,23 @@
 extends CharacterBody2D
 
-@export var speed: int = 300
+@export var speed: float = 100.0
 
-func _physics_process(delta: float) -> void:
-	# TODO
+@onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
+
+func _ready():
+	nav_agent.path_desired_distance = 4.0
+	nav_agent.target_desired_distance = 4.0
+	
+	set_physics_process(false)
+	await get_tree().process_frame
+	set_physics_process(true)
+
+func _physics_process(_delta):
+	nav_agent.target_position = get_global_mouse_position()
+	
+	if nav_agent.is_navigation_finished():
+		return
+	
+	var next_position: Vector2 = nav_agent.get_next_path_position()
+	velocity = global_position.direction_to(next_position) * speed
 	move_and_slide()
